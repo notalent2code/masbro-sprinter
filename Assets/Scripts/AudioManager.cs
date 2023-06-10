73d8e86b43327c4b3054bd8d7619b52c292837eb
notalent2code <rihlanrihlan24@gmail.com>
2023-06-10 19:46:44 +0700

    edit scripts and delete unnecessary comments

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -12,33 +12,24 @@ public class AudioManager : MonoBehaviour
     {
         foreach (Sound s in sounds)
         {
-            // Create a new AudioSource component
             s.source = gameObject.AddComponent<AudioSource>();
-            // Assign the clip to the AudioSource
             s.source.clip = s.clip;
-            // Set the volume
             s.source.volume = s.volume;
-            // Set the loop
             s.source.loop = s.loop;
         }
 
-        // Play the background music
         PlaySound("MainTheme");
     }
 
     public void PlaySound(string name)
     {
-        // Find the sound in the sounds array
         Sound s = System.Array.Find(sounds, sound => sound.name == name);
-        // Play the sound
         s.source.Play();
     }
 
     public void StopSound(string name)
     {
-        // Find the sound in the sounds array
         Sound s = System.Array.Find(sounds, sound => sound.name == name);
-        // Stop the sound
         s.source.Stop();
     }
 
